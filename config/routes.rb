7b0b690747fbfad17c80ef01a8d4a13ddd72c107
Doc Ritezel <pair+doc@ministryofvelocity.com>
2013-05-26 13:44:34 -0700

    Conditionally mount style guide engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Minifast::Application.routes.draw do
-  mount StyleGuide::Engine => "/style-guide"
+  mount StyleGuide::Engine => "/style-guide" if defined?(StyleGuide::Engine)
 
   resources :home, only: [:index]
   root to: "home#index"
