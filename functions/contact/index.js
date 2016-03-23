0f9dbc7b6d864ba1a7b307fe64cb048750e31492
Doc Ritezel <pair+doc@ministryofvelocity.com>
2016-03-22 17:13:01 -0700

    Add spec for lambda function

--- functions/contact/index.js
+++
@@ -1,3 +0,0 @@
-exports.handle = function(event, context) {
-  context.succeed({location: "https://www.ministryofvelocity.com/thanks.html"});
-}
---
+++ functions/contact/src/index.js
@@ -0,0 +1,5 @@
+import "babel-polyfill"
+
+export default (event, context) => {
+  context.succeed({location: "https://www.ministryofvelocity.com/thanks"});
+}
