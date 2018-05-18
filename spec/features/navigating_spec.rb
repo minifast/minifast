3c39a6ee94090139f8f4a3073420c5ed44d19603
Doc Ritezel <doc@ministryofvelocity.com>
2018-05-17 17:22:05 -0700

    Make the site accessible

--- spec/features/navigating_spec.rb
+++ spec/features/navigating_spec.rb
@@ -3,15 +3,41 @@
 require 'spec_helper'
 
 RSpec.describe 'Navigating the site', type: :feature do
-  it 'shows the welcome message', :js do
+  it 'describes the company', :js do
     visit '/'
+
+    expect(page).to have_content(/ministry of velocity/i)
 
     expect(page).to be_accessible.excluding('footer', '.minifast-process')
 
-    expect(page).to have_content(/pair programming/i)
+    within 'header' do
+      click_link 'Process'
+    end
+
+    expect(page).to be_accessible.excluding('footer')
+
+    within 'header' do
+      click_link 'Team'
+    end
+
+    expect(page).to be_accessible.excluding('footer')
+
+    within 'header' do
+      click_link 'Work'
+    end
+
+    expect(page).to be_accessible.excluding('footer')
+
+    within 'header' do
+      click_link 'Careers'
+    end
+
+    expect(page).to be_accessible.excluding('footer')
 
     within 'header' do
       click_link 'Contact'
     end
+
+    expect(page).to be_accessible.excluding('footer')
   end
 end
