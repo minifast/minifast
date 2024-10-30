8854b03ff372cef053e8aef858792f0e67b1ce2c
Doc Ritezel <pair+doc@ministryofvelocity.com>
2024-10-30 03:20:19 -0700

    Update dependencies

--- config.rb
+++ config.rb
@@ -3,14 +3,14 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-Dir.glob(File.expand_path('lib/**/*.rb', __dir__)).map(&:to_s).sort_by(&:length).each { |f| require(f) }
+Dir.glob(File.expand_path("lib/**/*.rb", __dir__)).map(&:to_s).sort_by(&:length).each { |f| require(f) }
 
-config[:contact_form_url] = ENV.fetch('GOOGLE_FORM_URL', 'http://example.com')
-config[:contact_name_id] = ENV.fetch('GOOGLE_FORM_NAME_ID', 'name')
-config[:contact_subject_id] = ENV.fetch('GOOGLE_FORM_SUBJECT_ID', 'subject')
-config[:google_maps_key] = ENV['GOOGLE_MAPS_KEY']
-config[:google_maps_private_key] = ENV['GOOGLE_MAPS_PRIVATE_KEY']
-config[:http_prefix] = ENV.fetch('RACK_BASE_URI', 'http://localhost:4567')
+config[:contact_form_url] = ENV.fetch("GOOGLE_FORM_URL", "http://example.com")
+config[:contact_name_id] = ENV.fetch("GOOGLE_FORM_NAME_ID", "name")
+config[:contact_subject_id] = ENV.fetch("GOOGLE_FORM_SUBJECT_ID", "subject")
+config[:google_maps_key] = ENV.fetch("GOOGLE_MAPS_KEY", nil)
+config[:google_maps_private_key] = ENV.fetch("GOOGLE_MAPS_PRIVATE_KEY", nil)
+config[:http_prefix] = ENV.fetch("RACK_BASE_URI", "http://localhost:4567")
 
 activate :asset_host, host: config[:http_prefix]
 activate :directory_indexes
@@ -18,42 +18,42 @@ activate :i18n, mount_at_root: :en
 activate :meta_tags
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = 'last 2 versions'
+  prefix.browsers = "last 2 versions"
 end
 
 activate :cloudfront do |cloudfront|
-  cloudfront.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  cloudfront.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  cloudfront.distribution_id = ENV['CLOUDFRONT_DISTRIBUTION_ID']
+  cloudfront.access_key_id = ENV.fetch("AWS_ACCESS_KEY_ID", nil)
+  cloudfront.secret_access_key = ENV.fetch("AWS_SECRET_ACCESS_KEY", nil)
+  cloudfront.distribution_id = ENV.fetch("CLOUDFRONT_DISTRIBUTION_ID", nil)
 end
 
 activate :s3_sync do |s3_sync|
-  s3_sync.bucket = ENV['BUCKET_NAME']
-  s3_sync.region = ENV['BUCKET_REGION']
-  s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  s3_sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.bucket = ENV.fetch("BUCKET_NAME", nil)
+  s3_sync.region = ENV.fetch("BUCKET_REGION", nil)
+  s3_sync.aws_access_key_id = ENV.fetch("AWS_ACCESS_KEY_ID", nil)
+  s3_sync.aws_secret_access_key = ENV.fetch("AWS_SECRET_ACCESS_KEY", nil)
   s3_sync.prefer_gzip = true
   s3_sync.path_style = true
   s3_sync.after_build = false
   s3_sync.reduced_redundancy_storage = false
   s3_sync.version_bucket = true
-  s3_sync.index_document = 'index.html'
-  s3_sync.error_document = '404.html'
+  s3_sync.index_document = "index.html"
+  s3_sync.error_document = "404.html"
 end
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
+set :images_dir, "images"
+set :fonts_dir, "fonts"
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
@@ -84,46 +84,43 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
-  activate :dotenv, env: '.env'
 
   activate :favicon_maker do |f|
-    f.template_dir = 'source/images/favicons'
+    f.template_dir = "source/images/favicons"
     f.icons = {
-      'large.png' => [
-        {icon: 'apple-touch-icon-180x180-precomposed.png'},
-        {icon: 'apple-touch-icon-152x152-precomposed.png'},
-        {icon: 'apple-touch-icon-144x144-precomposed.png'},
-        {icon: 'apple-touch-icon-120x120-precomposed.png'},
-        {icon: 'apple-touch-icon-114x114-precomposed.png'},
-        {icon: 'apple-touch-icon-76x76-precomposed.png'},
-        {icon: 'apple-touch-icon-72x72-precomposed.png'},
-        {icon: 'apple-touch-icon-60x60-precomposed.png'},
-        {icon: 'apple-touch-icon-57x57-precomposed.png'},
-        {icon: 'apple-touch-icon-precomposed.png', size: '57x57'},
-        {icon: 'apple-touch-icon.png', size: '57x57'},
-        {icon: 'favicon-196x196.png'},
-        {icon: 'favicon-160x160.png'},
-        {icon: 'favicon-96x96.png'},
-        {icon: 'mstile-70x70.png', size: '70x70'},
-        {icon: 'mstile-144x144.png', size: '144x144'},
-        {icon: 'mstile-150x150.png', size: '150x150'},
-        {icon: 'mstile-310x310.png', size: '310x310'},
-        {icon: 'mstile-310x150.png', size: '310x150'}
+      "large.png" => [
+        {icon: "apple-touch-icon-180x180-precomposed.png"},
+        {icon: "apple-touch-icon-152x152-precomposed.png"},
+        {icon: "apple-touch-icon-144x144-precomposed.png"},
+        {icon: "apple-touch-icon-120x120-precomposed.png"},
+        {icon: "apple-touch-icon-114x114-precomposed.png"},
+        {icon: "apple-touch-icon-76x76-precomposed.png"},
+        {icon: "apple-touch-icon-72x72-precomposed.png"},
+        {icon: "apple-touch-icon-60x60-precomposed.png"},
+        {icon: "apple-touch-icon-57x57-precomposed.png"},
+        {icon: "apple-touch-icon-precomposed.png", size: "57x57"},
+        {icon: "apple-touch-icon.png", size: "57x57"},
+        {icon: "favicon-196x196.png"},
+        {icon: "favicon-160x160.png"},
+        {icon: "favicon-96x96.png"},
+        {icon: "mstile-70x70.png", size: "70x70"},
+        {icon: "mstile-144x144.png", size: "144x144"},
+        {icon: "mstile-150x150.png", size: "150x150"},
+        {icon: "mstile-310x310.png", size: "310x310"},
+        {icon: "mstile-310x150.png", size: "310x150"}
       ],
-      'small.png' => [
-        {icon: 'favicon-32x32.png'},
-        {icon: 'favicon-16x16.png'},
-        {icon: 'favicon.png', size: '16x16'},
-        {icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16'}
+      "small.png" => [
+        {icon: "favicon-32x32.png"},
+        {icon: "favicon-16x16.png"},
+        {icon: "favicon.png", size: "16x16"},
+        {icon: "favicon.ico", size: "64x64,32x32,24x24,16x16"}
       ]
     }
   end
+end
 
-  activate :google_analytics do |ga|
-    ga.tracking_id = ENV.fetch('GOOGLE_ANALYTICS_TRACKING_ID')
-    ga.debug = false
-    ga.anonymize_ip = true
-    ga.domain_name = 'ministryofvelocity.com'
-    ga.allow_linker = true
+helpers do
+  def t(*args, **kwargs)
+    I18n.t(*args, **kwargs)
   end
 end
--- spec/support/axe.rb
+++ spec/support/axe.rb
@@ -1,3 +1,123 @@
 # frozen_string_literal: true
 
-require 'axe/rspec'
+require "capybara/cuprite"
+require "axe-rspec"
+
+module Capybara
+  class Session
+    alias_method :execute_async_script, :evaluate_async_script
+  end
+
+  module Cuprite
+    class Browser
+      alias_method :execute_async_script, :evaluate_async
+      alias_method :execute_script, :evaluate
+      alias_method :get, :goto
+
+      def manage
+        @manage ||= FakeManager.new(self)
+      end
+
+      def switch_to
+        @switch_to ||= FakeSwitcher.new(self)
+      end
+
+      def close
+        close_window(page.target_id)
+      end
+
+      class FakeSwitcher
+        def initialize(browser)
+          @browser = browser
+        end
+
+        def window(handle)
+          @browser.switch_to_window(handle)
+        end
+      end
+
+      class FakeManager
+        def initialize(browser)
+          @browser = browser
+        end
+
+        def timeouts
+          @timeouts ||= FakeTimeouts.new(@browser)
+        end
+      end
+
+      class FakeTimeouts
+        def initialize(browser)
+          @browser = browser
+        end
+
+        def page_load
+          @browser.timeout
+        end
+
+        def page_load=(timeout)
+          @browser.timeout = timeout
+        end
+      end
+    end
+  end
+end
+
+module Ferrum
+  class Frame
+    module Runtime
+      prepend(Module.new do
+        def evaluate_async(expression, *args)
+          # second argument is a timeout in milliseconds
+          super(expression, 10 * 1000, *args)
+        end
+      end)
+    end
+  end
+end
+
+module Axe
+  module API
+    class Run
+      FRAME_CONTEXT_SCRIPT = <<-JS
+        (function(context){
+          try {
+            return window.axe.utils.getFrameContexts(context);
+          } catch (err) {
+            return {
+              violations: [],
+              passes: [],
+              url: '',
+              timestamp: new Date().toString(),
+              errorMessage: err.message
+            };
+          }
+        })(arguments[0])
+      JS
+
+      def get_frame_context_script(page)
+        page.execute_script_fixed FRAME_CONTEXT_SCRIPT, @context
+      end
+
+      def store_chunk(page, chunk)
+        script = <<-JS
+        (function(chunk){
+          window.partialResults ??= '';
+          window.partialResults += chunk;
+        })(arguments[0])
+        JS
+        page.execute_script_fixed script, chunk
+      end
+
+      def axe_finish_run(page)
+        script = <<-JS
+        (function(){
+          const partialResults = JSON.parse(window.partialResults || '[]');
+          return axe.finishRun(partialResults);
+        })()
+        JS
+        page.execute_script_fixed script
+      end
+    end
+  end
+end
